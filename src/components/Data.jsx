2933a81eaf05d8686454043b02221435f8059968
dineshsaini777 <dinesh016622@gmail.com>
2024-06-25 11:12:09 -0400

    table ui

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,6 +1,84 @@
-import User from "../assets/image/svg"
-export const data = [{
-    src: User,
-    
-    
-}]
+import User from "../assets/image/svg/image.svg"
+import dots from "../assets/image/svg/dots.svg"
+
+export const data = [
+    {
+        src: User,
+        name: "John Doe",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+    {
+        src: User,
+        name: "John Doe",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+    {
+        src: User,
+        name: "John Doe",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+    {
+        src: User,
+        name: "John Doe",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+    {
+        src: User,
+        name: "John Doe",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+    {
+        src: User,
+        name: "John Doe",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+    {
+        src: User,
+        name: "John Doe ",
+        fullname: "john@example.com",
+        date: "01-01-2023",
+        city: "Hisar / Haryana",
+        active: "Active",
+        rup: "₹ 32,460.00",
+        image: dots,
+
+    },
+
+
+]
